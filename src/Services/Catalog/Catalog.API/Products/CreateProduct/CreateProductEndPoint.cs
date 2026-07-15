d18ef1d498b12173da89489372b5acbf7d0a2be6
Ravinder Sharma <verkaravinder@gmail.com>
2026-07-15 10:48:31 -0700

    First Minimal API Tested

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,8 +1,24 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndPoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndPoint : ICarterModule
     {
-        public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
-        public record CreateProductResponse(Guid Id);
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command, CancellationToken.None);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                 .ProducesProblem(StatusCodes.Status400BadRequest)
+                 .WithSummary("Create a Product")
+                 .WithDescription("Creates a new product in the catalog.");
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter();
 
 app.Run();
